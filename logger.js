a9e920f82dcba2ca4f5ce319adeb1d7ec3c0324b
Niv Gabay <nivg1992@gmail.com>
2023-12-06 20:01:39 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,12 +1,12 @@
 const pino = require('pino');
 
-module.exports = pino({
-    transport: {
-        target: 'pino-pretty',
-        level: process.env.LOG_LEVEL || "info",
-        options: {
+const transport = pino.transport({
+      target: 'pino-pretty',
+      level: process.env.LOG_LEVEL || "info",
+      options: {
           colorize: true,
           minimumLevel: process.env.LOG_LEVEL || "info"
-        }
-    },
-});
+      }
+})
+
+module.exports = pino({level: process.env.LOG_LEVEL || "info"}, transport);
