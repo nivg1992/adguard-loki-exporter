a79e03e7c21e3d7df8fa255f7dc0d9de50dc7730
Niv Gabay <nivg1992@gmail.com>
2023-12-06 19:54:03 +0200

    Update index.js

--- adguard/index.js
+++ adguard/index.js
@@ -66,6 +66,7 @@ class AdguardApi {
             if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
+                logger.error('Error Response');
                 logger.error(error.response.data);
                 logger.error(error.response.status);
                 logger.error(error.response.headers);
@@ -73,10 +74,11 @@ class AdguardApi {
                 // The request was made but no response was received
                 // `error.request` is an instance of XMLHttpRequest in the browser 
                 // and an instance of http.ClientRequest in node.js
+                logger.error('Error request');
                 logger.error(error.request);
               } else {
                 // Something happened in setting up the request that triggered an Error
-                logger.error('Error', error.message);
+                logger.error('Error', error.message, error.stack);
               }
         }
     }
