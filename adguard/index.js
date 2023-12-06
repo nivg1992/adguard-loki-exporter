51e34229a37631134a35e78dccf2fef2fdba7430
Niv Gabay <nivg1992@gmail.com>
2023-12-06 20:43:13 +0200

    Update index.js

--- adguard/index.js
+++ adguard/index.js
@@ -2,7 +2,7 @@ const axois = require('axios');
 const fs = require('fs');
 const logger = require('../logger');
 const MAX_PAGES = 1000;
-const CHUNK_SIZE = 5000;
+const CHUNK_SIZE = 500;
 
 function isValidDate(d) {
     return d instanceof Date && !isNaN(d);
